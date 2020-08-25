f2c0731bc708645ed07fef547c671a1e0aa8ba6d
Andrew Murray <atmurray@bellsouth.net>
2020-08-24 21:40:07 -0400

    basic practice for react fundamentals

--- src/App.js
+++ src/App.js
@@ -2,25 +2,56 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+  const list = [
+    {
+      title: 'React',
+      url: 'https:..reactjs.org./',
+      authors: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      authors: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
+
+  const List = () => {
+    list.map(el => (
+      <div key={el.objectID}>
+        <span>
+          <a href={el.url}>{el.title}</a>
+        </span>
+      </div>
+    ))
+  }
+
+  const handleChange = event => {
+    console.log(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> My Hacker Stories</h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+      <hr />
+
     </div>
-  );
+  )
+
 }
 
 export default App;
